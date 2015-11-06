fb73af52a762f04ba3e13a573a4d39a69d7a4e21
zhoudongyuan <zhoushiyuan0660@qq.com>
2015-11-06 18:38:50 +0800

    公司的新进展

--- app/src/main/java/com/example/administrator/givemeyounumber/MainActivity.java
+++ app/src/main/java/com/example/administrator/givemeyounumber/MainActivity.java
@@ -1,15 +1,39 @@
 package com.example.administrator.givemeyounumber;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.administrator.util.AnimatorUtil;
+
+public class MainActivity extends BaseActivity {
+    private ImageView iv_background,iv_people,iv_dialog;
+    private TextView tv_context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intiView();
+
+    }
+
+    private void intiView() {
+        iv_background = (ImageView)findViewById(R.id.iv_background);
+        iv_people = (ImageView)findViewById(R.id.iv_people);
+        iv_dialog = (ImageView)findViewById(R.id.iv_dialog);
+        tv_context =(TextView)findViewById(R.id.tv_context);
+
+        iv_dialog_effect();
+    }
+
+    private void iv_dialog_effect() {
+        AnimatorUtil.objectAnimatorTran(iv_dialog,"alpha",0f,1f,3000);
+
+        tv_context.setText("你好，我是一名IT工程师。");
+
+        AnimatorUtil.objectAnimatorTran(tv_context,"alpha",0f,1f,3000);
+
+
     }
 }
