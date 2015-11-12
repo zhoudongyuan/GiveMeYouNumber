112a75bd1330d4b89a0b0f726321b8329d2cdd32
zhoudongyuan <zhoushiyuan0660@qq.com>
2015-11-12 15:35:02 +0800

    缓一缓再写吧，目前没什么好的想法

--- app/src/main/java/com/example/administrator/givemeyounumber/MainActivity.java
+++ app/src/main/java/com/example/administrator/givemeyounumber/MainActivity.java
@@ -1,17 +1,12 @@
 package com.example.administrator.givemeyounumber;
 
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
 import android.os.Bundle;
-
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.administrator.util.AnimatorUtil;
-
 public class MainActivity extends BaseActivity {
     private ImageView iv_background,iv_people,iv_dialog;
-    private TextView tv_context1,tv_context2,tv_context3,tv_context4,tv_context5;
+    private TextView tv_context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,18 +18,13 @@ public class MainActivity extends BaseActivity {
         iv_background = (ImageView)findViewById(R.id.iv_background);
         iv_people = (ImageView)findViewById(R.id.iv_people);
         iv_dialog = (ImageView)findViewById(R.id.iv_dialog);
-        tv_context1 =(TextView)findViewById(R.id.tv_context1);
-        tv_context2 =(TextView)findViewById(R.id.tv_context2);
-        tv_context3 =(TextView)findViewById(R.id.tv_context3);
-        tv_context4 =(TextView)findViewById(R.id.tv_context4);
-        tv_context5 =(TextView)findViewById(R.id.tv_context5);
+        tv_context =(TextView)findViewById(R.id.tv_context);
 
         iv_dialog_effect();
     }
 
     private void iv_dialog_effect() {
-
-
+        
 
     }
 }
