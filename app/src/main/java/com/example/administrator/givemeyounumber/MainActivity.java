4b824a342b40c03d4dbba3a7b0e4a23480fdec5a
zhoudongyuan <zhoushiyuan0660@qq.com>
2015-11-24 16:52:24 +0800

    10

--- app/src/main/java/com/example/administrator/givemeyounumber/MainActivity.java
+++ app/src/main/java/com/example/administrator/givemeyounumber/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.administrator.givemeyounumber;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.administrator.util.AnimatorUtil;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener{
     private ImageView iv_background,iv_people,iv_dialog;
     private TextView tv_context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intiOntouth();
         intiView();
+    }
+
+    private void intiOntouth() {
+        iv_background.setOnClickListener(this);
+
     }
 
     private void intiView() {
@@ -28,5 +36,15 @@ public class MainActivity extends BaseActivity {
     private void iv_dialog_effect() {
         tv_context.setText("你好，我是一名腼腆的IT工程师");
         AnimatorUtil.objectAnimatorTranTogether(tv_context,"alpha",0f,1f,4000);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.iv_background:
+
+                break;
+        }
     }
 }
